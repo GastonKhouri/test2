35544b7b7587baedecf364549a59efb37516bfd4
Gaston Khouri <gastonkhouri@gmail.com>
2025-03-24 06:54:11 -0400

    First Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
 import { HeroesApp } from './HeroesApp';
 import { BrowserRouter } from 'react-router';
 import 'animate.css';
+import { HashRouter } from 'react-router';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <HeroesApp />
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot( document.getElementById( 'root' ) ).render(
+	<StrictMode>
+		<HashRouter>
+			<HeroesApp />
+		</HashRouter>
+	</StrictMode>,
+);
